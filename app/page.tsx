29525c806edcb8636863e218762af0ce6724e373
mehdad-hussain <hussain.mehdad1@gmail.com>
2023-11-04 15:26:02 +0600

    Add project information section to Home page

--- app/page.tsx
+++ app/page.tsx
@@ -9,6 +9,31 @@ export default function Home() {
             <section>
                 <GalleryUI />
             </section>
+
+
+            <section className="bg-gradient-to-r from-blue-400 to-green-400 p-6 rounded-lg shadow-lg">
+                <p className="mb-6 text-left">
+                    I created this project using Next.js, following the recommendation from the official React documentation. React no longer suggests using Create React App; instead, they recommend using Next.js and Remix. This framework choice offers numerous advantages for modern web development.
+                </p>
+                <div className="bg-white p-6 rounded-lg text-left">
+                    <h3 className="text-2xl font-bold mb-4">Key Technologies and Libraries</h3>
+                    <ul className="list-disc pl-5">
+                        <li>
+                            <strong>Next.js:</strong> Next.js serves as the foundation of the project, providing an excellent platform for building React applications.
+                        </li>
+                        <li>
+                            <strong>TypeScript:</strong> The project is developed using TypeScript, enhancing the development experience with strong typing and code validation.
+                        </li>
+                        <li>
+                            <strong>Tailwind CSS:</strong> Tailwind CSS is employed for styling, making it easy to create responsive and aesthetically pleasing user interfaces.
+                        </li>
+                        <li>
+                            <strong>Framer Motion:</strong> Framer Motion, a popular animation library, is utilized to add delightful animations and transitions to the user interface.
+                        </li>
+                    </ul>
+                </div>
+            </section>
+
         </main>
     );
 }
